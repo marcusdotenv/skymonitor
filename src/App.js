b0014e064ba2ef945ea72598cd3349d0fd476b35
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-02 12:04:42 -0300

    Adicionando primeira chamada a API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './styles.css';
 
+import api from './services/api'
+
+const apikey = "ysjFGTI56CVlOMbiAIGm7434IcnjzZ9S";
 
 
 function App() {
+  const[city, setCity] = useState('');
+  const[state, setState] = useState('');
+  async function getData(e){
+    e.preventDefault();
+    const data = await api.get(`/locations/v1/cities/search?apikey=${apikey}&q=${city+` `+state}`);
+    console.log(data);
+  }
+
   return (
     <div className="full-container">
-      <form>
+      <form onSubmit={getData}>
       <div className="application-section">
         <h1> Sky Monitor </h1>
         <h3>Digite sua Cidade</h3>
-        <input placeholder="Cidade"/> 
+        <input placeholder="Cidade" value={city} onChange={e => setCity(e.target.value)}/> 
+        <input placeholder="Cidade" value={state} onChange={e => setState(e.target.value)}/> 
         <button type="submit"> Vai! </button>
       </div>
       </form>
