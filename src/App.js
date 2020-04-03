db21672b2aa7c33a4ac87a59ba1d415610d229d7
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-03 10:51:21 -0300

    Funcionamento básico pronto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React,{useState} from 'react';
 import './styles.css';
-
 import api from './services/api'
-
 const apikey = "ysjFGTI56CVlOMbiAIGm7434IcnjzZ9S";
 
 
 function App() {
   const[city, setCity] = useState('');
   const[state, setState] = useState('');
+  const[temperature, setTemperature] = useState('');
+
   async function getData(e){
     e.preventDefault();
-    const data = await api.get(`/locations/v1/cities/search?apikey=${apikey}&q=${city+` `+state}`);
-    console.log(data);
-  }
+
+    const response = await api.get(`/locations/v1/cities/search?apikey=${apikey}&q=${city+` `+state}`);
+      try{
+        const locationKey = response.data[0].Key;
+        const locationConditions = await api.get(`/currentconditions/v1/${locationKey}?apikey=${apikey}`);
+        console.log(locationConditions);
+        const temperature = locationConditions.data[0].Temperature.Metric.Value;
+        setTemperature(temperature);
+
+      } catch(err){
+        alert('Localização Inválida!')
+      }
+    }
+
 
   return (
     <div className="full-container">
@@ -22,19 +33,16 @@ function App() {
         <h1> Sky Monitor </h1>
         <h3>Digite sua Cidade</h3>
         <input placeholder="Cidade" value={city} onChange={e => setCity(e.target.value)}/> 
-        <input placeholder="Cidade" value={state} onChange={e => setState(e.target.value)}/> 
+        <input placeholder="Estado" value={state} onChange={e => setState(e.target.value)}/> 
         <button type="submit"> Vai! </button>
       </div>
       </form>
       <div className="application-section">
         <h1> Condições Climáticas </h1>
         <div className="display-data">
-        <p>Umidade</p>
-        <p> 40 </p>
-        </div>
-        <div className="display-data">
-          <p>Temperatura</p>
-          <p> 30 </p>
+        <p>Temperatura</p>
+        <p>{temperature}</p>
+        <p> Condições </p>
         </div>
       </div>
     </div>
