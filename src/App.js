7a7fa48ed2104b1f2927deb3ef6c9a9a2cdd2bf3
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-03 22:44:38 -0300

    Hook useEffect para fazer chamada a API na inicilização

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import './styles.css';
 import api from './services/api'
 
 
-const apikey = "ysjFGTI56CVlOMbiAIGm7434IcnjzZ9S";
+const apikey = "gBbBC7bPAbmACOKzYRsUlwAmFg6945l6";
 
-const language = "pt-br";
-
+var storageCity;
+var storageState;
 
 function App() {
-  const[city, setCity] = useState(localStorage.getItem('last_city'));
-  const[state, setState] = useState(localStorage.getItem('last_state'));
+  if(localStorage.getItem('last_city')){
+    storageCity = localStorage.getItem('last_city');
+    storageState = localStorage.getItem('last_state');
+  } else{
+    storageCity = '';
+    storageState = '';
+  }
+
+  const[city, setCity] = useState(storageCity);
+  const[state, setState] = useState(storageState);
   const[temperature, setTemperature] = useState('');
   const[label, setLabel] = useState('');
 
@@ -34,6 +42,25 @@ function App() {
       }
     }
 
+    useEffect(()=>{
+      const getData = async()=>{
+        const response = await api.get(`/locations/v1/cities/search?apikey=${apikey}&q=${city+` `+state}`);
+          try{
+            const locationKey = response.data[0].Key;
+            const locationConditions = await api.get(`/currentconditions/v1/${locationKey}?apikey=${apikey}&language=pt-br`);
+            const label = locationConditions.data[0].WeatherText;
+            const temperature = locationConditions.data[0].Temperature.Metric.Value;
+
+            setTemperature(temperature);
+            setLabel(label);
+      }
+      catch(err){
+          console.log('')
+      }
+  }
+      getData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
 
   return (
     <div className="full-container">
